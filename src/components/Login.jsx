21ef859d69992555d8327448e4ffe10b1d7d335c
nicolascou <nicolascousillas1@gmail.com>
2023-04-17 11:30:28 +0200

    authentication implemented

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Dashboard from '../components/Dashboard';
 
 const Login = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(Boolean(localStorage.getItem('miranda-auth') === '1'));
+  const [userInput, setUserInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userInput === 'hola' && passwordInput === '1234') {
+      setIsAuthenticated(true);
+      localStorage.setItem('miranda-auth', '1');
+    }
+  }
+  
   return (
-    <div>Login</div>
+    <>
+      {
+      isAuthenticated ? <Dashboard /> :
+        <div className='login'>
+          <form method='post'>
+            <input 
+              value={userInput} onChange={e => setUserInput(e.target.value)} 
+              type="text" name="user" id="user" 
+            />
+            <input 
+              value={passwordInput} onChange={e => setPasswordInput(e.target.value)} 
+              type="password" name="password" id="password" 
+            />
+            <button onClick={(e) => handleSubmit(e)} type='submit'>Log In</button>
+          </form>
+        </div>
+      }
+    </>
   )
 }
 
