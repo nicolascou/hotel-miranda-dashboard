7b1e7efaa7942cae9cedd08c21beeb0c7d81b18b
nicolascou <nicolascousillas1@gmail.com>
2023-04-17 15:36:38 +0200

    start sidebar styles

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import hotelIcon from '../img/icons/hotel.svg';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <>
+      <div className='sidebar'>
+        <div className='sidebar__logo'>
+          <img width={45} height={45} src={hotelIcon} alt="" />
+          <div style={{ textAlign: 'left', marginLeft: '20px' }}>
+            <h1>travl</h1>
+            <p>Hotel Admin Dashboard</p>
+          </div>
+        </div>
+        <ul className='sidebar__nav'>
+          <li>Dashboard</li>
+          <li>Room</li>
+          <li>Bookings</li>
+          <li>Users</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
   )
 }
 
