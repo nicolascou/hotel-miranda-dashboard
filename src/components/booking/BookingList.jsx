b2f48c26ec04355544768d57cfe2fbb97b8cf077
nicolascou <nicolascousillas1@gmail.com>
2023-04-17 12:14:12 +0200

    json data for bookings and rooms

--- src/components/booking/BookingList.jsx
+++ src/components/booking/BookingList.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import bookings from '../../data/bookings.json';
 
 const BookingList = () => {
   return (
-    <div>BookingList</div>
+    <ul>
+      {
+        bookings.map((booking) => (
+          <li key={booking.id}>{booking.guest}, { booking.order_date }, {booking.check_in}, {booking.check_out}, {booking.room_type}, {booking.status}</li>
+        ))
+      }
+    </ul>
   )
 }
 
--- src/components/room/RoomList.jsx
+++ src/components/room/RoomList.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import rooms from '../../data/rooms.json';
 
 const RoomList = () => {
+
   return (
-    <div>RoomList</div>
+    <ul>
+      {
+        rooms.map((room) => (
+          <li key={room.id}>{room.room_name}, { room.bed_type }, {room.amenities}, {room.room_floor}, {room.rate}, {room.status}</li>
+        ))
+      }
+    </ul>
   )
 }
 
