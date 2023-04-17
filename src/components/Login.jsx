5003c411857cfac0c940b04c1252199fe45149ae
nicolascou <nicolascousillas1@gmail.com>
2023-04-17 15:24:18 +0200

    end styles for login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Dashboard from '../components/Dashboard';
 import hotelIcon from '../img/icons/hotel.svg';
-import styled from 'styled-components';
+import { Button } from './styled';
 
 const Login = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(Boolean(localStorage.getItem('miranda-auth') === '1'));
@@ -30,17 +30,21 @@ const Login = () => {
               </div>
             </div>
             <form method='post' className='login__card__form'>
-              <label htmlFor="user">User:</label>
-              <input className='login__card__form__input' 
-                value={userInput} onChange={e => setUserInput(e.target.value)} 
-                type="text" name="user" id="user" placeholder='hola'
-              />
-              <label htmlFor="password">Password:</label>
-              <input className='login__card__form__input' 
-                value={passwordInput} onChange={e => setPasswordInput(e.target.value)} 
-                type="password" name="password" id="password" placeholder='1234'
-              />
-              <button onClick={(e) => handleSubmit(e)} type='submit'>Log In</button>
+              <div className='login__card__form__box'>
+                <label htmlFor="user">User:</label>
+                <input className='login__card__form__input' 
+                  value={userInput} onChange={e => setUserInput(e.target.value)} 
+                  type="text" name="user" id="user" placeholder='hola'
+                />
+              </div> 
+              <div className='login__card__form__box'>
+                <label htmlFor="password">Password:</label>
+                <input className='login__card__form__input' 
+                  value={passwordInput} onChange={e => setPasswordInput(e.target.value)} 
+                  type="password" name="password" id="password" placeholder='1234'
+                />
+              </div>    
+              <Button onClick={(e) => handleSubmit(e)} type='submit'>Log In</Button>
             </form>
           </div>
         </div>
---
+++ src/components/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border-radius: 12px;
+  cursor: pointer;
+  padding 5px 20px;
+  border: none;
+  outline: none;
+`
