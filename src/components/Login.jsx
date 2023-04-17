7967909f48546797e93d83605cce2ad58e062587
nicolascou <nicolascousillas1@gmail.com>
2023-04-17 13:33:11 +0200

    add styles for login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Dashboard from '../components/Dashboard';
 import hotelIcon from '../img/icons/hotel.svg';
+import styled from 'styled-components';
 
 const Login = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(Boolean(localStorage.getItem('miranda-auth') === '1'));
@@ -20,18 +21,28 @@ const Login = () => {
       {
       isAuthenticated ? <Dashboard /> :
         <div className='login'>
-          <img width={100} height={100} src={hotelIcon} alt="" />
-          <form method='post'>
-            <input 
-              value={userInput} onChange={e => setUserInput(e.target.value)} 
-              type="text" name="user" id="user" 
-            />
-            <input 
-              value={passwordInput} onChange={e => setPasswordInput(e.target.value)} 
-              type="password" name="password" id="password" 
-            />
-            <button onClick={(e) => handleSubmit(e)} type='submit'>Log In</button>
-          </form>
+          <div className='login__card'>
+            <div className='login__card__logo'>
+              <img width={50} height={50} src={hotelIcon} alt="" />
+              <div style={{ textAlign: 'left', marginLeft: '20px' }}>
+                <h1>travl</h1>
+                <p>Hotel Admin Dashboard</p>
+              </div>
+            </div>
+            <form method='post' className='login__card__form'>
+              <label htmlFor="user">User:</label>
+              <input className='login__card__form__input' 
+                value={userInput} onChange={e => setUserInput(e.target.value)} 
+                type="text" name="user" id="user" placeholder='hola'
+              />
+              <label htmlFor="password">Password:</label>
+              <input className='login__card__form__input' 
+                value={passwordInput} onChange={e => setPasswordInput(e.target.value)} 
+                type="password" name="password" id="password" placeholder='1234'
+              />
+              <button onClick={(e) => handleSubmit(e)} type='submit'>Log In</button>
+            </form>
+          </div>
         </div>
       }
     </>
