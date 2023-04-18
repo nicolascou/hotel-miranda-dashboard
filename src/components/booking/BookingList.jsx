7b597a85ba9ac202510692f98892b3d8039141db
nicolascou <nicolascousillas1@gmail.com>
2023-04-18 15:34:39 +0200

    status button classes

--- src/components/booking/BookingList.jsx
+++ src/components/booking/BookingList.jsx
@@ -27,32 +27,47 @@ const BookingList = () => {
           <p className='bookings__table__row__item'>Status</p>
         </div>
         <ul style={{ listStyle: 'none' }}>
-          {bookings.slice(0, 8).map((b) => (
-            <div key={b.id} className='bookings__table__row'>
-              <div className='bookings__table__row__item'>
-                <div className='bookings__table__row__item__photo'></div>
-                <div>
-                  <p className='weight-500'>{b.guest}</p>
-                  <p className='bookings__table__row__item__id'>{b.guest_id}</p>
+          {bookings.slice(0, 8).map((b) => {
+            let statusClass;
+            if (b.status === 'Booked') {
+              statusClass = 'bookings__table__row__item__status--green';
+            } else if (b.status === 'Refund') {
+              statusClass  = 'bookings__table__row__item__status--red'
+            } else if (b.status === 'Pending') {
+              statusClass  = 'bookings__table__row__item__status--gray'
+            } else if (b.status === 'Canceled') {
+              statusClass  = 'bookings__table__row__item__status--black'
+            }
+              
+            return (  
+              <div key={b.id} className='bookings__table__row'>
+                <div className='bookings__table__row__item'>
+                  <div className='bookings__table__row__item__photo'></div>
+                  <div>
+                    <p className='weight-500'>{b.guest}</p>
+                    <p className='bookings__table__row__item__id'>{b.guest_id}</p>
+                  </div>
                 </div>
-              </div>
-              <p className='bookings__table__row__item'>{b.order_date}</p>
-              <div className='bookings__table__row__item' style={{ flexDirection: 'column' }}>
-                <p className='weight-500'>{b.check_in}</p>
-                <p className='small-text'>12.00 PM</p>
-              </div>
-              <div className='bookings__table__row__item' style={{ flexDirection: 'column' }}>
-                <p className='weight-500'>{b.check_out}</p>
-                <p className='small-text'>9.00AM</p>
-              </div>
-              <div className='bookings__table__row__item'>
-                <button className='bookings__table__row__item__btn'>View Notes</button>
-              </div>
-              <p className='bookings__table__row__item weight-500'>{b.room_type}</p>
-              <p className='bookings__table__row__item'>{b.status}</p>
-              <i className='fa-solid fa-ellipsis-vertical bookings__table__row__ellipsis'></i>
-            </div>
-          ))}
+                <p className='bookings__table__row__item'>{b.order_date}</p>
+                <div className='bookings__table__row__item' style={{ flexDirection: 'column' }}>
+                  <p className='weight-500'>{b.check_in}</p>
+                  <p className='small-text'>12.00 PM</p>
+                </div>
+                <div className='bookings__table__row__item' style={{ flexDirection: 'column' }}>
+                  <p className='weight-500'>{b.check_out}</p>
+                  <p className='small-text'>9.00AM</p>
+                </div>
+                <div className='bookings__table__row__item'>
+                  <button className='bookings__table__row__item__btn'>View Notes</button>
+                </div>
+                <p className='bookings__table__row__item weight-500'>{b.room_type}</p>
+                <div className='bookings__table__row__item'>
+                  <p className={`bookings__table__row__item__status ${statusClass}`}>
+                    {b.status}
+                  </p>
+                </div>
+                <i className='fa-solid fa-ellipsis-vertical bookings__table__row__ellipsis'></i>
+              </div> )})}
         </ul>
       </div>
     </div>
