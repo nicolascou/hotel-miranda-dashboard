58b0028768acbfdff9b791b00c904537528bfcb3
nicolascou <nicolascousillas1@gmail.com>
2023-04-18 16:24:23 +0200

    add active css class to pagination

--- src/components/booking/BookingList.jsx
+++ src/components/booking/BookingList.jsx
@@ -93,15 +93,21 @@ const BookingList = () => {
         </ul>
       </div>
       <div className='bookings__bottom'>
-        <p className='bookings__bottom__text'>Showing 5 of 102 Data</p>
+        <p className='bookings__bottom__text'>Showing 8 of 102 Data</p>
         <div className='bookings__bottom__pagination'>
           <button onClick={() => handlePagination(pagination-1)} className='bookings__bottom__pagination__btn1'>Prev</button>
-          <button onClick={() => handlePagination(1)} 
-            className='bookings__bottom__pagination__btn2 bookings__bottom__pagination__btn2--active'
+          <button onClick={() => handlePagination(1)} className={`bookings__bottom__pagination__btn2 
+            ${pagination === 1 ? 'bookings__bottom__pagination__btn2--active' : ''}`}
           >1</button>
-          <button onClick={() => handlePagination(2)} className='bookings__bottom__pagination__btn2'>2</button>
-          <button onClick={() => handlePagination(3)} className='bookings__bottom__pagination__btn2'>3</button>
-          <button onClick={() => handlePagination(4)} className='bookings__bottom__pagination__btn2'>4</button>
+          <button onClick={() => handlePagination(2)} className={`bookings__bottom__pagination__btn2 
+            ${pagination === 2 ? 'bookings__bottom__pagination__btn2--active' : ''}`}
+          >2</button>
+          <button onClick={() => handlePagination(3)} className={`bookings__bottom__pagination__btn2 
+            ${pagination === 3 ? 'bookings__bottom__pagination__btn2--active' : ''}`}
+          >3</button>
+          <button onClick={() => handlePagination(4)} className={`bookings__bottom__pagination__btn2 
+            ${pagination === 4 ? 'bookings__bottom__pagination__btn2--active' : ''}`}
+          >4</button>
           <button onClick={() => handlePagination(pagination+1)} className='bookings__bottom__pagination__btn1'>Next</button>
         </div>
       </div>
