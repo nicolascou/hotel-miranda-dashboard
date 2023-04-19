015acdd1a49faf5d6c6194562c324bde22aa8dd3
nicolascou <nicolascousillas1@gmail.com>
2023-04-19 11:45:00 +0200

    styles in details for head

--- src/components/booking/BookingDetails.jsx
+++ src/components/booking/BookingDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import allBookings from '../../data/bookings.json';
+import room1 from '../../img/rooms-3.jpg';
 import { useParams } from 'react-router-dom';
 
 const BookingDetails = () => {
@@ -13,10 +14,37 @@ const BookingDetails = () => {
   return (
     <div className='booking-details'>
       <div className='booking-details__left'>
-        {booking.guest}
-      </div>
+        <div className='booking-details__row'>
+          <div className='booking-details__left__photo'>
+            <i className='fa-solid fa-user'></i>
+          </div>
+          <div className='booking-details__left__main'>
+            <h2>{booking.guest}</h2>
+            <p>ID {booking.guest_id}</p>
+            <div style={{ display: 'flex' }}>
+              <button className='booking-details__left__main__phone-btn'>
+                <i className='fa-solid fa-phone'></i>
+              </button>
+              <button className='booking-details__left__main__message-btn'>
+                <i className='fa-solid fa-message'></i>
+                Send message
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='booking-details__row'>
+          <div className='booking-details__check'>
+            <p>Check In</p>
+            <p>{booking.check_in}</p>
+          </div>
+          <div className='booking-details__check'>
+            <p>Check Out</p>
+            <p>{booking.check_out}</p>
+          </div>
+        </div>
+      </div>  
       <div className='booking-details__right'>
-        <i className='fa-solid fa-user booking-details__right__i'></i>
+        <img className='booking-details__right__img' src={room1} alt="" />
         <div className='booking-details__right__text'>
           <h3 className='booking-details__right__text__title'>Bed Room</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facere earum expedita dolore, fuga nobis debitis sint illo. Nisi recusandae eveniet dolor adipisci quod sapiente accusantium sunt fugit ipsum molestias?</p>
