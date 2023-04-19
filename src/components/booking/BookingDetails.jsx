1c289547225943fb82bf5a6a7848af0b0cd4f18b
nicolascou <nicolascousillas1@gmail.com>
2023-04-19 11:58:52 +0200

    more styles in details

--- src/components/booking/BookingDetails.jsx
+++ src/components/booking/BookingDetails.jsx
@@ -33,15 +33,27 @@ const BookingDetails = () => {
           </div>
         </div>
         <div className='booking-details__row'>
-          <div className='booking-details__check'>
+          <div className='booking-details__left__check'>
             <p>Check In</p>
             <p>{booking.check_in}</p>
           </div>
-          <div className='booking-details__check'>
+          <div className='booking-details__left__check'>
             <p>Check Out</p>
             <p>{booking.check_out}</p>
           </div>
         </div>
+        <div className='booking-details__left__bar'></div>
+        <div className='booking-details__row'>
+          <div className='booking-details__left__room'>
+            <p>Room Info</p>
+            <p>{booking.room_type}</p>
+          </div>
+          <div className='booking-details__left__room'>
+            <p>Price</p>
+            <p>$145<span> /night</span></p>
+          </div>
+        </div>
+        <p className='booking-details__left__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
       </div>  
       <div className='booking-details__right'>
         <img className='booking-details__right__img' src={room1} alt="" />
