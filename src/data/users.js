7243beabfdfabc480dbee4e61fa9b05ae205c8e4
nicolascou <nicolascousillas1@gmail.com>
2023-04-26 12:22:27 +0200

    add json data for users

---
+++ src/data/users.js
@@ -0,0 +1,93 @@
+import nicoPhoto from '../img/profile-pic.jpg' 
+
+
+export const users = [
+    {
+        "id": 1,
+        "full_name": "Nicolás Cousillas",
+        "username": "nico",
+        "photo": nicoPhoto,
+        "position": "Manager",
+        "email": "nicolascousillas1@gmail.com",
+        "start_date": "2022-04-01",
+        "state": "active",
+        "password": "1234"
+    },
+    {
+        "id": 2,
+        "full_name": "Alice Johnson",
+        "username": "alicej",
+        "photo": undefined,
+        "position": "Receipt",
+        "email": "alicejohnson@example.com",
+        "start_date": "2023-02-15",
+        "state": "active",
+        "password": "p@ssword123"
+    },
+    {
+        "id": 3,
+        "full_name": "Bob Smith",
+        "username": "bobsmith",
+        "photo": undefined,
+        "position": "Room Service",
+        "email": "bobsmith@example.com",
+        "start_date": "2023-03-10",
+        "state": "inactive",
+        "password": "mysecret"
+    },
+    {
+        "id": 4,
+        "full_name": "Lucas Torres",
+        "username": "lucast",
+        "photo": null,
+        "position": "Manager",
+        "email": "lucast@example.com",
+        "start_date": "2022-04-05",
+        "state": "active",
+        "password": "mysecurepass"
+    },
+    {
+        "id": 5,
+        "full_name": "Maria Rodriguez",
+        "username": "maria_rod",
+        "photo": null,
+        "position": "Receipt",
+        "email": "maria.rodriguez@example.com",
+        "start_date": "2023-05-10",
+        "state": "active",
+        "password": "mypassword"
+    },
+    {
+        "id": 6,
+        "full_name": "Luisa Perez",
+        "username": "luisap",
+        "photo": null,
+        "position": "Room Service",
+        "email": "luisa.perez@example.com",
+        "start_date": "2023-06-15",
+        "state": "inactive",
+        "password": "12345678"
+    },
+    {
+        "id": 7,
+        "full_name": "Mark Johnson",
+        "username": "markj",
+        "photo": null,
+        "position": "Receipt",
+        "email": "mark.johnson@example.com",
+        "start_date": "2023-07-20",
+        "state": "active",
+        "password": "password123"
+    },
+    {
+        "id": 8,
+        "full_name": "Samantha Lee",
+        "username": "samlee",
+        "photo": null,
+        "position": "Room Service",
+        "email": "samlee@example.com",
+        "start_date": "2023-08-25",
+        "state": "active",
+        "password": "mysupersecret"
+    },
+]
