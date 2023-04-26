2f0c429ca4bf8c78a0bdd752b32c2545a7bd5edf
nicolascou <nicolascousillas1@gmail.com>
2023-04-26 15:50:27 +0200

    remove unnecessary items from header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
-import profilePic from '../../img/profile-pic.jpg';
 
 const Header = ({ hideSidebar, setHideSidebar }) => {
   const location = useLocation();
@@ -21,13 +20,8 @@ const Header = ({ hideSidebar, setHideSidebar }) => {
         <h2 className='header__title'>{location.pathname.split('/')[1] || 'Dashboard'}</h2>
       </div>
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div className='header__search'>
-          <input type="text" />
-          <i className='fa-solid fa-search'></i>
-        </div>
         <i className='header__icon fa-regular fa-envelope'></i>
         <i className='header__icon fa-regular fa-bell'></i>
-        <img className='header__img' src={profilePic} alt="profile" />
         <div className='header__bar'></div>
         <i onClick={handleLogout} className='header__icon fa-solid fa-right-from-bracket'></i>
       </div>
