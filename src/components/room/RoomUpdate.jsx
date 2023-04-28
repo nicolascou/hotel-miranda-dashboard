e6b8ce4ff93e0c1dcd01751bffae7d45a448144f
nicolascou <nicolascousillas1@gmail.com>
2023-04-28 10:30:47 +0200

    basic styles in update

--- src/components/room/RoomUpdate.jsx
+++ src/components/room/RoomUpdate.jsx
@@ -36,56 +36,66 @@ const RoomUpdate = () => {
   }
   
   return (
-    <div className='room-create'>
-      <h2 className='room-create__title'>Add New Room</h2>
-      <form className='room-create__form' onSubmit={(e) => handleSubmit(e)}>
-        <select value={roomType} onChange={(e) => setRoomType(e.target.value)}>
-          <option value="Single Bed">Single Bed</option>
-          <option value="Double Bed">Double Bed</option>
-          <option value="Double Luxury">Double Luxury</option>
-        </select>
-        <div>
-          <label htmlFor="name">Room name</label>
-          <input type="name" id='name' value={roomName} onChange={(e) => setRoomName(e.target.value)} />
+    <div className='create'>
+      <h2 className='create__title'>Edit Room</h2>
+      <form className='create__form' onSubmit={(e) => handleSubmit(e)}>
+        <div className='create__form__row'>
+          <select className='create__form__row__cell' value={roomType} onChange={(e) => setRoomType(e.target.value)}>
+            <option value="Single Bed">Single Bed</option>
+            <option value="Double Bed">Double Bed</option>
+            <option value="Double Luxury">Double Luxury</option>
+          </select>
+          <div className='create__form__row__cell'>
+            <label htmlFor="name">Room name</label>
+            <input type="name" id='name' value={roomName} onChange={(e) => setRoomName(e.target.value)} />
+          </div>
         </div>
-        <div>
-          <label htmlFor="description">Description</label>
-          <textarea id="description" cols="30" rows="10" value={roomDescription} onChange={(e) => setRoomDescription(e.target.value)}></textarea>
-        </div>
-        <div>
-          <label htmlFor="price">Price</label>
-          <input type="number" value={roomPrice} onChange={(e) => setRoomPrice(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="offer">Offer Price</label>
-          <input type="number" id='offer' value={roomOffer} onChange={(e) => setRoomOffer(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="cancel">Cancellation Policy</label>
-          <textarea cols="30" rows="10" id='cancel'></textarea>
-        </div>
-        <div>
-          <label htmlFor="amenities">Amenities</label>
-          <div>
-            <div>
-              <label htmlFor="shower">Shower</label>
-              <input type="checkbox" id="shower" />
+        <div className='create__form__row'>
+          <div className='create__form__row'>
+            <div className='create__form__row__cell'>
+              <label htmlFor="description">Description</label>
+              <textarea id="description" cols="30" rows="10" value={roomDescription} onChange={(e) => setRoomDescription(e.target.value)}></textarea>
             </div>
-            <div>
-              <label htmlFor="ac">AC</label>
-              <input type="checkbox" id="ac" />
+          </div>
+          <div className='create__form__row__second-col'>
+            <div className='create__form__row__cell'>
+              <label htmlFor="price">Price</label>
+              <input type="number" value={roomPrice} onChange={(e) => setRoomPrice(e.target.value)} />
             </div>
-            <div>
-              <label htmlFor="wifi">Wifi</label>
-              <input type="checkbox" id="wifi" />
-            </div>
-            <div>
-              <label htmlFor="ledtv">LED TV</label>
-              <input type="checkbox" id="ledtv" />
+            <div className='create__form__row__cell'>
+              <label htmlFor="offer">Offer Price</label>
+              <input type="number" id='offer' value={roomOffer} onChange={(e) => setRoomOffer(e.target.value)} />
             </div>
           </div>
         </div>
-        <button type='submit'>Create Room</button>
+        <div className='create__form__row'>
+          <div className='create__form__row__cell'>
+            <label htmlFor="cancel">Cancellation Policy</label>
+            <textarea cols="30" rows="10" id='cancel'></textarea>
+          </div>
+          <div className='create__form__row__cell'>
+            <label htmlFor="amenities">Amenities</label>
+            <div>
+              <div>
+                <label htmlFor="shower">Shower</label>
+                <input type="checkbox" id="shower" />
+              </div>
+              <div>
+                <label htmlFor="ac">AC</label>
+                <input type="checkbox" id="ac" />
+              </div>
+              <div>
+                <label htmlFor="wifi">Wifi</label>
+                <input type="checkbox" id="wifi" />
+              </div>
+              <div>
+                <label htmlFor="ledtv">LED TV</label>
+                <input type="checkbox" id="ledtv" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <button type='submit'>Update Room</button>
       </form>
     </div>
   )
