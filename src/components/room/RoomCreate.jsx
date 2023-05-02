cc5b1454699f76c19e77a320926c353f960efd93
nicolascou <nicolascousillas1@gmail.com>
2023-05-02 16:47:04 +0200

    add create form styles

--- src/components/room/RoomCreate.jsx
+++ src/components/room/RoomCreate.jsx
@@ -14,7 +14,6 @@ const RoomCreate = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(formRef.current);
-    console.log(formData.get('bed_type'));
     const room = {
       "name": formData.get('name'),
       "bed_type": formData.get('bed_type'),
@@ -31,24 +30,18 @@ const RoomCreate = () => {
     <div className='create'>
       <h2 className='create__title'>Add New Room</h2>
       <form ref={formRef} className='create__form' onSubmit={(e) => handleSubmit(e)}>
-        <div className='create__form__row'>
-          <select name='bed_type' className='create__form__row__cell'>
-            <option value="Single Bed">Single Bed</option>
-            <option value="Double Bed">Double Bed</option>
-            <option value="Double Luxury">Double Luxury</option>
-          </select>
-          <div className='create__form__row__cell'>
-            <label htmlFor="name">Room name</label>
-            <input name='name' type="name" id='name' />
-          </div>
-        </div>
-        <div className='create__form__row'>
-          <div className='create__form__row__cell'>
-            <label htmlFor="description">Description</label>
-            <textarea name='description' id="description" cols="30" rows="10"></textarea>
-          </div>
-          <div className='create__form__row__second-col'>
-            <div>
+        <div className='create__form__grid'>
+          <div className='create__form__column'>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="name">Room name</label>
+              <input name='name' type="name" id='name' />
+            </div>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="price">Price</label>
+              <input name='price' type="number" />
+            </div>
+            <div className='create__form__radio'>
+              <i>Discount?</i>
               <label htmlFor="offerYes">Yes</label>
               <input type="radio" name='offer' id='offerYes' onChange={() => setOffer(true)} checked={offer} />
               <label htmlFor="offerNo">No</label>
@@ -56,45 +49,50 @@ const RoomCreate = () => {
             </div>
             {
               offer &&
-              <div className='create__form__row__cell'>
-                <label htmlFor="discount">Discount</label>
+              <div className='create__form__column__cell'>
+                <label className='weight-600' htmlFor="discount">Discount</label>
                 <input name='discount' type="number" id='discount' />
               </div>
             }
-            <div className='create__form__row__cell'>
-              <label htmlFor="price">Price</label>
-              <input name='price' type="number" />
-            </div>
-          </div>
-        </div>
-        <div className='create__form__row'>
-          <div className='create__form__row__cell'>
-            <label htmlFor="cancel">Cancellation Policy</label>
-            <textarea name='cancellation' cols="30" rows="10" id='cancel'></textarea>
-          </div>
-          <div className='create__form__row__cell'>
-            <label htmlFor="amenities">Amenities</label>
-            <div>
+            <select name='bed_type' className='create__form__column__cell weight-600'>
+              <option value="Single Bed">Single Bed</option>
+              <option value="Double Bed">Double Bed</option>
+              <option value="Double Luxury">Double Luxury</option>
+            </select>
+            <div className='create__form__column__cell create__form__amenities'>
+              <label className='weight-600' htmlFor="amenities">Amenities</label>
               <div>
-                <label htmlFor="shower">Shower</label>
-                <input type="checkbox" id="shower" />
-              </div>
-              <div>
-                <label htmlFor="ac">AC</label>
-                <input type="checkbox" id="ac" />
-              </div>
-              <div>
-                <label htmlFor="wifi">Wifi</label>
-                <input type="checkbox" id="wifi" />
-              </div>
-              <div>
-                <label htmlFor="ledtv">LED TV</label>
-                <input type="checkbox" id="ledtv" />
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="shower">Shower</label>
+                  <input type="checkbox" id="shower" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="ac">AC</label>
+                  <input type="checkbox" id="ac" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="wifi">Wifi</label>
+                  <input type="checkbox" id="wifi" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="ledtv">LED TV</label>
+                  <input type="checkbox" id="ledtv" />
+                </div>
               </div>
             </div>
           </div>
+          <div className='create__form__column'>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="description">Description</label>
+              <textarea name='description' id="description" cols="30" rows="10"></textarea>
+            </div>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="cancel">Cancellation Policy</label>
+              <textarea name='cancellation' cols="30" rows="10" id='cancel'></textarea>
+            </div>
+          </div>
         </div>
-        <button type='submit'>Create Room</button>
+        <button type='submit' className='create__form__btn'>Create Room</button>
       </form>
     </div>
   )
