5521737d178e617ab1d424021223783fe031716c
nicolascou <nicolascousillas1@gmail.com>
2023-05-02 17:15:13 +0200

    styles in room update form

--- src/components/room/RoomUpdate.jsx
+++ src/components/room/RoomUpdate.jsx
@@ -36,63 +36,59 @@ const RoomUpdate = () => {
     <div className='create'>
       <h2 className='create__title'>Edit Room</h2>
       <form ref={formRef} className='create__form' onSubmit={(e) => handleSubmit(e)}>
-        <div className='create__form__row'>
-          <select name='bed_type' className='create__form__row__cell' defaultValue={room.bed_type}>
-            <option value="Single Bed">Single Bed</option>
-            <option value="Double Bed">Double Bed</option>
-            <option value="Double Luxury">Double Luxury</option>
-          </select>
-          <div className='create__form__row__cell'>
-            <label htmlFor="name">Room name</label>
-            <input name='name' type="name" id='name' defaultValue={room.name} />
-          </div>
-        </div>
-        <div className='create__form__row'>
-          <div className='create__form__row'>
-            <div className='create__form__row__cell'>
-              <label htmlFor="description">Description</label>
-              <textarea name='description' id="description" cols="30" rows="10" defaultValue={room.description || ''}></textarea>
+        <div className='create__form__grid'>
+          <div className='create__form__column'>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="name">Room name</label>
+              <input name='name' type="name" id='name' defaultValue={room.name} />
             </div>
-          </div>
-          <div className='create__form__row__second-col'>
-            <div className='create__form__row__cell'>
-              <label htmlFor="price">Price</label>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="price">Price</label>
               <input name='price' type="number" defaultValue={room.rate} />
             </div>
-            <div className='create__form__row__cell'>
-              <label htmlFor="offer">Offer Price</label>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="offer">Offer Price</label>
               <input name='offer' type="number" id='offer' defaultValue={room.offer || Math.floor(room.rate / 1.5)} />
             </div>
-          </div>
-        </div>
-        <div className='create__form__row'>
-          <div className='create__form__row__cell'>
-            <label htmlFor="cancel">Cancellation Policy</label>
-            <textarea name='cancellation' cols="30" rows="10" id='cancel'></textarea>
-          </div>
-          <div className='create__form__row__cell'>
-            <label htmlFor="amenities">Amenities</label>
-            <div>
+            <select name='bed_type' className='create__form__column__cell weight-600' defaultValue={room.bed_type}>
+              <option value="Single Bed">Single Bed</option>
+              <option value="Double Bed">Double Bed</option>
+              <option value="Double Luxury">Double Luxury</option>
+            </select>
+            <div className='create__form__column__cell create__form__amenities'>
+              <label className='weight-600' htmlFor="amenities">Amenities</label>
               <div>
-                <label htmlFor="shower">Shower</label>
-                <input type="checkbox" id="shower" />
-              </div>
-              <div>
-                <label htmlFor="ac">AC</label>
-                <input type="checkbox" id="ac" />
-              </div>
-              <div>
-                <label htmlFor="wifi">Wifi</label>
-                <input type="checkbox" id="wifi" />
-              </div>
-              <div>
-                <label htmlFor="ledtv">LED TV</label>
-                <input type="checkbox" id="ledtv" />
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="shower">Shower</label>
+                  <input type="checkbox" id="shower" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="ac">AC</label>
+                  <input type="checkbox" id="ac" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="wifi">Wifi</label>
+                  <input type="checkbox" id="wifi" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="ledtv">LED TV</label>
+                  <input type="checkbox" id="ledtv" />
+                </div>
               </div>
             </div>
           </div>
+          <div className='create__form__column'>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="description">Description</label>
+              <textarea name='description' id="description" cols="30" rows="10" defaultValue={room.description || ''}></textarea>
+            </div>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="cancel">Cancellation Policy</label>
+              <textarea name='cancellation' cols="30" rows="10" id='cancel'></textarea>
+            </div>
+          </div>
         </div>
-        <button type='submit'>Update Room</button>
+        <button className='create__form__btn' type='submit'>Update Room</button>
       </form>
     </div>
   )
