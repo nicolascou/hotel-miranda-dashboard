cae8bea1ecabcebc7b45d47026e30fdf64330183
nicolascou <nicolascousillas1@gmail.com>
2023-05-02 13:40:15 +0200

    add useRef to roomUpdate form

--- src/components/room/RoomUpdate.jsx
+++ src/components/room/RoomUpdate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { updateRoom } from '../../features/rooms/roomThunks';
 import room3 from '../../img/rooms-3.jpg';
@@ -9,25 +9,22 @@ const RoomUpdate = () => {
   const { data } = useSelector(state => state.room);
   const room = data.roomList.find(({ id }) => id === Number(params.id));
   
-  const [roomName, setRoomName] = useState(room.name);
-  const [roomType, setRoomType] = useState(room.bed_type);
-  const [roomPrice, setRoomPrice] = useState(room.rate);
-  const [roomOffer, setRoomOffer] = useState(room.offer || Math.floor(room.rate / 1.5));
-  const [roomDescription, setRoomDescription] = useState(room.description || '');
+  const formRef = useRef(null);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
   
   const handleSubmit = (e) => {
     e.preventDefault();
+    const formData = new FormData(formRef.current);
     const room = {
       "id": Number(params.id),
-      "name": roomName,
-      "bed_type": roomType,
+      "name": formData.get('name'),
+      "bed_type": formData.get('bed_type'),
       "photo": room3,
-      "rate": roomPrice,
-      "offer": roomOffer,
-      "description": roomDescription,
+      "rate": formData.get('price'),
+      "offer": formData.get('offer'),
+      "description": formData.get('description'),
       "amenities": ["Wifi", "Towels", "LED TV"],
       "status": "Available",
     }
@@ -38,40 +35,40 @@ const RoomUpdate = () => {
   return (
     <div className='create'>
       <h2 className='create__title'>Edit Room</h2>
-      <form className='create__form' onSubmit={(e) => handleSubmit(e)}>
+      <form ref={formRef} className='create__form' onSubmit={(e) => handleSubmit(e)}>
         <div className='create__form__row'>
-          <select className='create__form__row__cell' value={roomType} onChange={(e) => setRoomType(e.target.value)}>
+          <select name='bed_type' className='create__form__row__cell' defaultValue={room.bed_type}>
             <option value="Single Bed">Single Bed</option>
             <option value="Double Bed">Double Bed</option>
             <option value="Double Luxury">Double Luxury</option>
           </select>
           <div className='create__form__row__cell'>
             <label htmlFor="name">Room name</label>
-            <input type="name" id='name' value={roomName} onChange={(e) => setRoomName(e.target.value)} />
+            <input name='name' type="name" id='name' defaultValue={room.name} />
           </div>
         </div>
         <div className='create__form__row'>
           <div className='create__form__row'>
             <div className='create__form__row__cell'>
               <label htmlFor="description">Description</label>
-              <textarea id="description" cols="30" rows="10" value={roomDescription} onChange={(e) => setRoomDescription(e.target.value)}></textarea>
+              <textarea name='description' id="description" cols="30" rows="10" defaultValue={room.description || ''}></textarea>
             </div>
           </div>
           <div className='create__form__row__second-col'>
             <div className='create__form__row__cell'>
               <label htmlFor="price">Price</label>
-              <input type="number" value={roomPrice} onChange={(e) => setRoomPrice(e.target.value)} />
+              <input name='price' type="number" defaultValue={room.rate} />
             </div>
             <div className='create__form__row__cell'>
               <label htmlFor="offer">Offer Price</label>
-              <input type="number" id='offer' value={roomOffer} onChange={(e) => setRoomOffer(e.target.value)} />
+              <input name='offer' type="number" id='offer' defaultValue={room.offer || Math.floor(room.rate / 1.5)} />
             </div>
           </div>
         </div>
         <div className='create__form__row'>
           <div className='create__form__row__cell'>
             <label htmlFor="cancel">Cancellation Policy</label>
-            <textarea cols="30" rows="10" id='cancel'></textarea>
+            <textarea name='cancellation' cols="30" rows="10" id='cancel'></textarea>
           </div>
           <div className='create__form__row__cell'>
             <label htmlFor="amenities">Amenities</label>
