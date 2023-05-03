82a421e58ab0efc6cfe25322fc94346d7d225f2c
nicolascou <nicolascousillas1@gmail.com>
2023-05-03 11:10:15 +0200

    add data to user object in submit

--- src/components/user/UserCreate.jsx
+++ src/components/user/UserCreate.jsx
@@ -1,8 +1,88 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux';
+import { createUser } from '../../features/users/userThunks';
+import { useNavigate } from 'react-router-dom';
+import moment from 'moment';
 
 const UserCreate = () => {
+  const formRef = useRef(null);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+    const user = {
+      "full_name": formData.get('full_name'),
+      "username": formData.get('username'),
+      "position": formData.get('position'),
+      "email": formData.get('email'),
+      "phone": formData.get('phone'),
+      "description": formData.get('description'),
+      "start_date": moment().format('YYYY-MM-DD'),
+      "state": formData.get('state'),
+      "password": formData.get('password')
+    }
+    dispatch(createUser(user))
+    navigate('/users');
+  }
+  
   return (
-    <div>UserCreate</div>
+    <div className='create'>
+      <h2 className='create__title'>Add New Room</h2>
+      <form ref={formRef} className='create__form' onSubmit={(e) => handleSubmit(e)}>
+        <div className='create__form__grid'>
+          <div className='create__form__column'>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="name">Room name</label>
+              <input name='name' type="name" id='name' />
+            </div>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="price">Price</label>
+              <input name='price' type="number" />
+            </div>
+            <select name='bed_type' className='create__form__column__cell weight-600'>
+              <option value="Single Bed">Single Bed</option>
+              <option value="Double Bed">Double Bed</option>
+              <option value="Double Luxury">Double Luxury</option>
+            </select>
+            <div className='create__form__column__cell create__form__amenities'>
+              <label className='weight-600' htmlFor="amenities">Amenities</label>
+              <div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="shower">Shower</label>
+                  <input type="checkbox" id="shower" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="ac">AC</label>
+                  <input type="checkbox" id="ac" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="wifi">Wifi</label>
+                  <input type="checkbox" id="wifi" />
+                </div>
+                <div className='create__form__amenities__box'>
+                  <label htmlFor="ledtv">LED TV</label>
+                  <input type="checkbox" id="ledtv" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='create__form__column'>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="description">Description</label>
+              <textarea name='description' id="description" cols="30" rows="10"></textarea>
+            </div>
+            <div className='create__form__column__cell'>
+              <label className='weight-600' htmlFor="cancel">Cancellation Policy</label>
+              <textarea name='cancellation' cols="30" rows="10" id='cancel'></textarea>
+            </div>
+          </div>
+        </div>
+        <button type='submit' className='create__form__btn'>Create Room</button>
+      </form>
+    </div>
   )
 }
 
